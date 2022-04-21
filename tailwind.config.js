eeae614d608a121e9c59f3d7e37f8b057557e47c
KakuElvis <kakuelvis@gmail.com>
2022-04-21 00:44:34 +0000

    update the UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html","./pages/**/*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
         'hero': "url('/img/images.jpeg')"
-      }
+      },
+      colors: {
+        'main': '#0A182E',
+        'second-main': '#081325',
+        'heading': '#CDD7F6',
+        'text': '#8993B0',
+        'hover-text': '#65FFDB',
+        
+      },
     },
   },
   plugins: [],
